bf2c7aae9b29bba04ccf5906fa0f2c491475172c
Михаил Торбин <mltor8@gmail.com>
2023-07-08 23:22:23 -0400

    TestBase.java updated

--- RoadTrafficSigns/src/test/java/TestBase.java
+++ RoadTrafficSigns/src/test/java/TestBase.java
@@ -1,2 +1,38 @@
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import pages.HomePage;
+
+import java.util.concurrent.TimeUnit;
+
 public class TestBase {
+    protected static WebDriver driver;
+    protected static WebDriverWait wait;
+
+    @BeforeAll
+    public static void setUp() {
+        System.setProperty("webdriver.chrome.driver", "drivers//chromedriver.exe");
+        var options = new ChromeOptions();
+        options.addArguments("--start-maximized");
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        wait = new WebDriverWait(driver, 20);
+        driver.manage().timeouts().implicitlyWait(7, TimeUnit.SECONDS);
+    }
+
+    @AfterAll
+    public static void tearDown() {
+        driver.quit();
+    }
+
+    @BeforeEach
+    public void navigateToHomePage() {
+        var homePage = new HomePage(driver, wait);
+
+        homePage.open();
+    }
 }
