f1e42948ecc5a29e2e5c4e8537a92a66b301352f
Михаил Торбин <mltor8@gmail.com>
2023-07-08 23:21:38 -0400

    HomePageTests.java updated

--- RoadTrafficSigns/src/test/java/HomePageTests.java
+++ RoadTrafficSigns/src/test/java/HomePageTests.java
@@ -1,2 +1,24 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import pages.HomePage;
+
 public class HomePageTests extends TestBase {
+    /* Navigate to Home Page
+     *
+     */
+    @Test
+    public void homePage_ClickOnCustomTrafficSignsImageBox_CustomTrafficSignsPageAppears() {
+        // arrange
+        var homePage = new HomePage(driver, wait);
+
+        // act
+        homePage.customTrafficSignsImageBox.click();
+
+        // assert
+        Assertions.assertTrue(homePage.isPageTitleCustomTrafficSigns(), "Custom Traffic Signs page doesn't appear");
+        var expectedPageHeading = "Custom Traffic Signs";
+        Assertions.assertEquals(expectedPageHeading, homePage.getCustomTrafficSignsPageHeadingText(),
+                "Custom Traffic Signs heading doesn't appear");
+    }
+
 }
