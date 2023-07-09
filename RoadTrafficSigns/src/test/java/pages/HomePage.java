e696169b9b7be02c8b60f02f0c31eb894b12f7f1
Михаил Торбин <mltor8@gmail.com>
2023-07-08 23:21:09 -0400

    HomePage.java updated

--- RoadTrafficSigns/src/test/java/pages/HomePage.java
+++ RoadTrafficSigns/src/test/java/pages/HomePage.java
@@ -1,5 +1,45 @@
 package pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 public class HomePage {
+    private WebDriver driver;
+    private WebDriverWait wait;
+    private String url = "https://www.roadtrafficsigns.com/";
+
+    @FindBy(xpath = "//*[@id='department_grid_25059']/div[1]//div[@class='dept_imagebox']")
+    public WebElement customTrafficSignsImageBox;
+
+    @FindBy(xpath = "//*[@class='ss_dept_main_heading ']//h1")
+    public WebElement customTrafficSignsPageHeading;
+
+//    @FindBy(xpath = "")
+//    public WebElement ;
+//
+//    @FindBy(xpath = "")
+//    public WebElement ;
+//
+//    @FindBy(xpath = "")
+//    public WebElement ;
+
+    public HomePage(WebDriver driver, WebDriverWait wait) {
+        this.driver = driver;
+        this.wait = wait;
+        PageFactory.initElements(driver, this);
+    }
+
+    public void open() {
+        driver.navigate().to(url);
+    }
+
+    public boolean isPageTitleCustomTrafficSigns() { return driver.getTitle().contains("Custom Traffic Signs"); }
+
+    public String getCustomTrafficSignsPageHeadingText() {
+        return customTrafficSignsPageHeading.getText();
+    }
 
 }
